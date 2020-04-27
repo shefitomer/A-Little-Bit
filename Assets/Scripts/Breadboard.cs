e844b655c6bd4b5312fb348a52e01a6cde3a254f
shefitomer <shefitomer@gmail.com>
2020-04-27 23:54:05 +0300

    Revert "Added a breadboard slot script"
    This reverts commit 6f4f1ba0ec829ac6ce55fe0b287c85e080809378.

--- Assets/Scripts/Breadboard.cs
+++ Assets/Scripts/Breadboard.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Breadboard : Machine
 {
-    public static Breadboard instance;
-
-    void Awake()
-    {
-        if(instance != null)
-        {
-            Debug.LogWarning("More than one breadboard created!");
-            return;
-        }
-        instance = this;
-    }
-
     public Breadboard(GameObject prefab, Vector2 position) : base("Breadboard", prefab, position)
     {
 
@@ -24,5 +12,6 @@ public class Breadboard : Machine
     override public void open()
     {
         Debug.Log("Main Open");
+        this.GO.AddComponent<BreadboardGUI>();
     }
 }
--- Assets/Scripts/BreadboardUI.cs
+++ Assets/Scripts/BreadboardUI.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class BreadboardUI : MonoBehaviour
 {
-	Breadboard breadboard;
     // Start is called before the first frame update
     void Start()
     {
-        breadboard = Breadboard.instance;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void UpdateUI()
-    {
-    	Debug.Log("Updating UI");
     }
 }
